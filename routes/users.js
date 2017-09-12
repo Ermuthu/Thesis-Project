9d8ebbf7b61610e8736f71102d9de12970c16866
Dan Martin <dmartss13@gmail.com>
2017-09-11 21:35:40 -0400

    successfully fetching users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+
+  // And insert something like this instead:
+  res.json([{
+  	id: 1,
+  	username: "dan"
+  }, {
+  	id: 2,
+  	username: "man"
+  }]);
 });
 
 module.exports = router;

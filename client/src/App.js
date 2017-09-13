9375ca05c2079b132eab3b98da6c4c3965973450
Dan Martin <dmartss13@gmail.com>
2017-09-13 16:19:47 -0400

    rendering test spotify component

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Spotify from './components/Spotify/Spotify';
 
 class App extends Component {
-  state = {users: []}
 
   componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
+        {/* <h1>Users</h1> */}
+          <Spotify />
       </div>
     );
   }
---
+++ client/src/components/Spotify/Spotify.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Spotify extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Spotify</h1>
+                <form action="">
+                    <input type="search" value=""/>
+                    <input type ="submit" value="Create"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Spotify;

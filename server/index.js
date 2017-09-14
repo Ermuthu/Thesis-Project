7e82c87437a5b1a2b7a9e3c5241c6e4157faaf0b
Dan Martin <dmartss13@gmail.com>
2017-09-14 16:32:54 -0400

    access tokens

--- server/index.js
+++ server/index.js
@@ -3,27 +3,36 @@ const app = express();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const SpotifyStrategy = require("passport-spotify").Strategy;
-const keys = require('./config/keys');
+const keys = require("./config/keys");
 
 passport.use(new SpotifyStrategy());
 passport.use(new GoogleStrategy());
 
-
 passport.use(
-  new GoogleStrategy({
-    googleClientID: keys.googleClientID,
-    googleClientSecret: keys.googleClientSecret
-  })
+  new GoogleStrategy(
+    {
+      googleClientID: keys.googleClientID,
+      googleClientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
 );
 
 passport.use(
-  new SpotifyStrategy({
-    spotifyClientID: keys.spotifyClientID,
-    spotifyClientSecret: keys.spotifyClientSecret,
-    callbackURL: "http://localhost:3000/auth/spotify/callback"
-  })
+  new SpotifyStrategy(
+    {
+      spotifyClientID: keys.spotifyClientID,
+      spotifyClientSecret: keys.spotifyClientSecret,
+      callbackURL: "http://localhost:3000/auth/spotify/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
 );
-
 
 const PORT = process.env.PORT || 3000;
 //express tells node to listen on port 3000

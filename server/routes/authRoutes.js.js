da994a5f8a45b0bdfdd3ec4b7eb5e9333f534da2
Dan Martin <dmartss13@gmail.com>
2017-09-14 18:28:26 -0400

    move spotify oauth routes from index to authRoutes

--- server/routes/authRoutes.js.js
+++ server/routes/authRoutes.js.js
@@ -6,3 +6,13 @@ app.get(
   );
   app.get("/auth/google/callback", passport.authenticate("google"));
   
+
+  app.get(
+    "/auth/spotify",
+    passport.authenticate("spotify", {
+      scope: ["user-read-email", "user-read-private"],
+      showDialog: true
+    })
+  );
+  app.get("/auth/spotify/callback", passport.authenticate("spotify"));
+  

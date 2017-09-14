820b1dea162fb2bdd7ac61b69e6655d23133cbb0
Dan Martin <dmartss13@gmail.com>
2017-09-14 15:39:37 -0400

    require passports

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const SpotifyStrategy = require("passport-spotify").Strategy;
 
 // when incoming "/" request (req), respond (res) send json data
 // arrow function called automatically every time request comes in

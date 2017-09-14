5098f29493fc7d9b2a46542e2b24dd7cc400f644
Dan Martin <dmartss13@gmail.com>
2017-09-14 18:39:47 -0400

    call app.get routes from auth-routes

--- server/index.js
+++ server/index.js
@@ -1,31 +1,10 @@
 const express = require("express");
+const authRoutes = require('./routes/auth-routes');
+require("./services/passport");
+
 const app = express();
 
-
-
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: keys.spotifyClientID,
-      clientSecret: keys.spotifyClientSecret,
-      callbackURL: "http://localhost:3000/auth/spotify/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile:', profile);
-    }
-  )
-);
-app.get(
-  "/auth/spotify",
-  passport.authenticate("spotify", {
-    scope: ["user-read-email", "user-read-private"],
-    showDialog: true
-  })
-);
-app.get("/auth/spotify/callback", passport.authenticate("spotify"));
-
+authRoutes(app);
 
 const PORT = process.env.PORT || 3000;
 //express tells node to listen on port 3000

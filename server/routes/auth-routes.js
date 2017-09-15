d1d44e545f4159b26ac70ed6c0fcb7fe2ff71d70
Dan Martin <dmartss13@gmail.com>
2017-09-14 23:47:11 -0400

    logout user route set up

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -21,4 +21,9 @@ module.exports = app => {
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
 };

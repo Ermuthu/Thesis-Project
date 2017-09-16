96d490ac13043c1ec2ec368031dcad566fb3ad70
Dan Martin <dmartss13@gmail.com>
2017-09-15 23:14:00 -0400

    connect props  to header

--- server/client/src/components/Header.jsx
+++ server/client/src/components/Header.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
   render() {
+    console.log(this.props);
     return (
       <nav>
-          <div>
-              <a>Login with Google</a>
-              <a>Login with Spotify</a>
+        <div>
+          <a>Login with Google</a>
+          <a>Login with Spotify</a>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+// function mapStateToProps(state) {
+//     return { auth: state.auth };
+// }
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);

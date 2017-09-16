04520dca9b848604f9c0800131cf8dca0856d8cc
Dan Martin <dmartss13@gmail.com>
2017-09-15 21:05:40 -0400

    testing routes

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./Header";
+const Home = () => <h1> Home Page </h1>
+const User = () => <h1> User Page </h1>
 
 class App extends Component {
   render() {
     return (
+      <div>
         <BrowserRouter>
+          <div>
             <Header />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/user" component={User} />
+          </div>
         </BrowserRouter>
+      </div>
     );
   }
 }

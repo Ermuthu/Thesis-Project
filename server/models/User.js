1727d81257cb55f167740a241e46eddafd3a8ecb
Dan Martin <dmartss13@gmail.com>
2017-09-17 19:18:27 -0400

    trying to get new data in to mongo

--- server/models/User.js
+++ server/models/User.js
@@ -5,14 +5,20 @@ const userSchema = new Schema({
   created: { type: Date, default: Date.now },
   googleId: String,
   spotifyId: String,
-  user: String,
-  access_token: String,
+  // user: String,
+  accessToken: String,
+  token_type: String,
+  scope: String,
+  expires_in: Number,
   refresh_token: String,
+  display_name: String,
+  accessToken: String,
   name: String,
+  email: String,
   alltracks: [],
   image: [],
   playlists: [],
-  ownPlaylist: []
+  ownPlaylist: [],
 });
 
 mongoose.model("users", userSchema);

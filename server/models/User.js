8ddf8676bb6920f4c9e335b5bd953ab7225d3ffe
Dan Martin <dmartss13@gmail.com>
2017-09-16 22:33:16 -0400

    more info for mongo

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,17 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  created: { type: Date, default: Date.now },
   googleId: String,
-  spotifyId: String
+  spotifyId: String,
+  user: String,
+  access_token: String,
+  refresh_token: String,
+  name: String,
+  alltracks: [],
+  image: [],
+  playlists: [],
+  ownPlaylist: []
 });
 
 mongoose.model("users", userSchema);

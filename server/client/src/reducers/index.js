ca6d59ddf853502ebe04f7469c596fa76733e531
Dan Martin <dmartss13@gmail.com>
2017-09-18 03:20:20 -0400

    redux form reducer

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from "./authReducer";
 import spotifyReducer from "./spotifyReducer";
 
 export default combineReducers({
   auth: authReducer,
+  form: reduxForm,
   search: spotifyReducer
 });

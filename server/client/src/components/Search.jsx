f108c844c4307161305f64fec1a1f7090ea8c99b
Dan Martin <dmartss13@gmail.com>
2017-09-17 22:41:24 -0400

    redux form is console logging input

--- server/client/src/components/Search.jsx
+++ server/client/src/components/Search.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
 class Search extends Component {
   render() {
     return (
       <div>
-        Search!
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field type="text" name="searchTrack" component="input" />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
 }
 
-export default Search;
+export default reduxForm({
+  form: "Search"
+})(Search);

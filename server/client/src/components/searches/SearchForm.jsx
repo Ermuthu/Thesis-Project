a46e38fc2e8ae67f63719c1cd568ea0849c39359
Dan Martin <dmartss13@gmail.com>
2017-09-17 23:00:51 -0400

    change search to search form

--- server/client/src/components/searches/SearchForm.jsx
+++ server/client/src/components/searches/SearchForm.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import SearchField from "./SearchField";
 
 class Search extends Component {
+  renderFields() {
+    return (
+      <div>
+        <Field type="text" name="title" component={SearchField} />
+      </div>
+    );
+  }
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="searchTrack" component="input" />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>

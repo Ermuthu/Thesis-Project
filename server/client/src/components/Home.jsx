277e6c928f01b9d64a9b93a535496c5f3da578df
Dan Martin <dmartss13@gmail.com>
2017-09-19 16:08:26 -0400

    having fun with switch statements for home page

--- server/client/src/components/Home.jsx
+++ server/client/src/components/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import Header from "./Header";
 
-const Home = () => {
+class Home extends Component {
+  renderHeader() {
+    switch (this.props.auth) {
+      case null:
+        return "loading...";
+      default:
+        return (
+          <div>
+            <Header />
+            <a href="/api/logout">
+              <i className="large material-icons right">exit_to_app</i>Logout
+            </a>
+          </div>
+        );
+    }
+  }
+  render() {
     return (
-        <div>
-            Meditate 
+      <div>
+        Welcome to your #1 ~ meditation destination ~
+        <div className="waves-effect waves-light">
+          <Link to="/spotify">Spotify Search</Link>
+          <Link to="/surveys">
+            Surveys
+            <i className="material-icons">event_note</i>
+          </Link>
+          <Link to="/youtube">Search Youtube</Link>
         </div>
+      </div>
     );
-};
+  }
+}
 
-export default Home;
+function mapStateToProps({ auth, actions }) {
+  return { auth, actions };
+}
+
+export default connect(mapStateToProps)(Home);

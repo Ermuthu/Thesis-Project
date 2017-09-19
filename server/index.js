5fb0adc89667cf1e7e3abc4613b6b46f650d2d61
Dan Martin <dmartss13@gmail.com>
2017-09-19 14:53:29 -0400

    backend routes

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 require("./models/User");
+require("./models/Surveys");
 require("./services/passport");
 
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
@@ -14,7 +16,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    maxAge: 10 * 5 * 1000,
     keys: [keys.cookieKey]
   })
 );
@@ -22,18 +24,28 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/auth-routes")(app);
+require("./routes/billing-routes")(app);
+require("./routes/survey-routes")(app);
 require("./routes/spotify-routes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(`${PORT} is da bombski.. suhh~?`)
-    }
+app.listen(PORT, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`${PORT} is da bombski.. suhh~?`);
+  }
 });
 
 app.get("*", function(req, res) {
-  res.status(404).send({ message: "Oops! Not found." });
+  res.status(404).send({ message: "ugh errors man.. amirite tho roflmao" });
 });
-

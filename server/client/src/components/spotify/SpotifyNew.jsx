8d81dafa4a9d68b35bb806a68b87d896deb07646
Dan Martin <dmartss13@gmail.com>
2017-09-20 01:14:54 -0400

    spotify views

--- server/client/src/components/spotify/SpotifyNew.jsx
+++ server/client/src/components/spotify/SpotifyNew.jsx
@@ -1,8 +1,14 @@
 // this will show search options and search results
 import React, { Component } from "react";
 import SpotifyForm from "./SpotifyForm";
+import SpotifyResult from "./SpotifyResult";
 
 class SearchNew extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { results: true };
+  }
+
   render() {
     return (
       <div>
---
+++ server/client/src/components/spotify/SpotifyResult.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpotifyResult = () => {
+  return <div>results</div>;
+};
+
+export default SpotifyResult;

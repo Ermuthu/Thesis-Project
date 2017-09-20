4366618bd9aa7e14b46231553410112bafa1f3a6
Dan Martin <dmartss13@gmail.com>
2017-09-20 00:12:08 -0400

    redux form stuff

--- server/client/src/components/spotify/SpotifyForm.jsx
+++ server/client/src/components/spotify/SpotifyForm.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { reduxForm, Field } from "redux-form";
 
 class SpotifyForm extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(search => console.log(search))}>
+          <Field type="text" name="spotifySearch" component="input" />
+          <button type="submit" className="teal btn-flat right white-text">
+            Search!
+            <i className="material-icons right">search</i>
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default reduxForm()(SpotifyForm);
+export default reduxForm({
+  form: "spotifyForm"
+})(SpotifyForm);

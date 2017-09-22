baa6fedf8c66eba6f8a96109350168730ad97374
Dan Martin <dmartss13@gmail.com>
2017-09-22 17:26:06 -0400

    fetch song and artist actions

--- client/src/actions/index.jsx
+++ client/src/actions/index.jsx
@@ -1,18 +1,66 @@
 import axios from "axios";
 import {
   FETCH_USER,
-  SEARCH_SONG,
-  FETCH_SONG,
+  FETCH_ARTIST,
   ADD_SEARCH,
-  ADD_LOG
+  ADD_LOG,
+  CLEAR_SEARCH,
+  FETCH_SONG
 } from "./constants";
-
-// const baseUrl = "https://api.spotify.com/";
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+const BASE_URL = "https://api.spotify.com/v1/search?";
+
+export const fetchArtist = (input, state, auth) => async dispatch => {
+  const FETCH_URL = `${BASE_URL}q=${input}&type=artist&limit=1`;
+  // console.log("FETCH_URL", FETCH_URL);
+  console.log("searchType");
+  const headers = {
+    Authorization: "Bearer " + state.accessToken
+  };
+  const res = await axios.get(FETCH_URL, {
+    method: "get",
+    headers: headers
+  });
+  dispatch({ type: FETCH_ARTIST, payload: res.data });
+  // console.log(res.data.artists.items);
+  return {
+    type: FETCH_ARTIST,
+    payload: res.data
+  };
+  // console.log(res);
+};
+
+export const fetchSong = (input, state, auth) => async dispatch => {
+  const FETCH_URL = `${BASE_URL}q=${input}&type=track`;
+  // console.log("FETCH_URL", FETCH_URL);
+  console.log("search", input);
+  const headers = {
+    Authorization: "Bearer " + state.accessToken
+  };
+  const res = await axios.get(FETCH_URL, {
+    method: "get",
+    headers: headers
+  });
+  dispatch({ type: FETCH_SONG, payload: res.data });
+  // console.log(res.data.artists.items);
+  return {
+    type: FETCH_SONG,
+    payload: res.data
+  };
+  // console.log(res);
+};
+
+export function clearSearch() {
+  return {
+    type: CLEAR_SEARCH,
+    data: {}
+  };
+}
 
 export const addLogs = text => {
   const action = {
@@ -20,15 +68,6 @@ export const addLogs = text => {
     text
   };
   // console.log("action in addLog", action);
-  return action;
-};
-
-export const searchSong = text => {
-  const action = {
-    type: SEARCH_SONG,
-    text
-  };
-  // console.log("action in searchSong", action);
   return action;
 };
 
@@ -41,36 +80,17 @@ export const addSearch = text => {
   return action;
 };
 
-export const fetchSong = (song, state, auth) => async dispatch => {
-  console.log("song of the query -> ", song);
-  console.log(state.accessToken);
-  const headers = {
-    Authorization: "Bearer " + state.accessToken
-  };
-  console.log("headers", headers);
-  // console.log(accessToken);
-  // console.log("search", `${song}`);
-  const res = await axios.get(
-    `https://api.spotify.com/v1/search?q=${song}&type=artist`,
-    {
-      method: "get",
-      headers: headers
-    }
-  );
-  dispatch({ type: FETCH_SONG, payload: res.data });
-  console.log(res.data);
-
-  return {
-    type: FETCH_SONG,
-    payload: res
-  };
-};
+// console.log("song of the query -> ", song);
+// console.log(state.accessToken);
+// console.log("headers", headers);
+// console.log(accessToken);
+// console.log("song", `${song}`);
 
 //   "Content-Type": "application/json",
 //   Authorization: "Bearer " + this.getToken()
 // }
 
-// export const fetchSong = (accessToken, values) => {
+// export const fetchArtist = (accessToken, values) => {
 //   const search = `v1/search?q=${values}&type=artist&limit=1`;
 //   return dispatch => {
 // const headers = { Authorization: "Bearer " + window.accessToken };

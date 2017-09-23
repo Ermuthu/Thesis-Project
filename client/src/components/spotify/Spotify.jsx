2985f3a4d70516da4616fc3514c6f157873034f3
Dan Martin <dmartss13@gmail.com>
2017-09-23 01:11:58 -0400

    neatoo cleanup

--- client/src/components/spotify/Spotify.jsx
+++ client/src/components/spotify/Spotify.jsx
@@ -3,15 +3,13 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Button } from "react-bootstrap";
 import * as actions from "../../actions";
+import SpotifyArtist from "./SpotifyArtist";
+import SpotifySong from "./SpotifySong";
 
 class Spotify extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "",
-      input: ""
-    };
-  }
+  state = {
+    input: ""
+  };
 
   submitArtist() {
     this.props.actions.fetchArtist(this.state.input, this.state.accessToken);
@@ -21,39 +19,15 @@ class Spotify extends Component {
     this.props.actions.fetchSong(this.state.input, this.state.accessToken);
   }
 
-  renderArtist() {
-    const { spotify } = this.props;
-    console.log("spotify after render", spotify.artists.items[0]);
-    console.log("spotify images", spotify.artists.items[0].images);
-    const track = spotify.artists.items;
-    console.log(track);
-    return (
-      <div className="row">
-        {track.map((item, index) => (
-          <div className="row-two" key={index}>
-            {item.images.length > 0 ? (
-              <a href={item.external_urls.spotify}>
-                <img src={item.images[0].url} alt="" />
-              </a>
-            ) : (
-              <a href={item.external_urls.spotify}>listen</a>
-            )}
-            <p>{item.name}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
   render() {
     // console.log("props at render", this.props);
-    // const { acessToken } = this.props;
     // console.log("auth");
     return (
       <div>
         <div className="search">
           <input
             className="form-control"
-            placeholder="search by input, artist"
+            placeholder="search by artist"
             onChange={event =>
               this.setState({
                 accessToken: this.props.auth,
@@ -69,7 +43,6 @@ class Spotify extends Component {
         <Button
           type="button"
           className="btn btn-success"
-          data-filter="button"
           onClick={() => this.submitArtist()}
         >
           "Search for an Artist!"
@@ -81,18 +54,21 @@ class Spotify extends Component {
         >
           "Search for a Song!"
         </Button>
-        {this.props.spotify ? this.renderArtist() : " "}
+        {this.props.spotify ? <SpotifyArtist /> : ""}
+        {this.props.song ? <SpotifySong /> : ""}
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  console.log("state", state);
-  console.log("spotify state", state.spotify);
+  // console.log("state", state);
+  // console.log("spotify state", state.spotify);
+  // console.log("song state", state.song);
   return {
     auth: state.auth,
-    spotify: state.spotify
+    spotify: state.spotify,
+    song: state.song
   };
 }
 

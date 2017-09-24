6e6de517ecb92a163046bfd051ae91abfb2dc9ff
Dan Martin <dmartss13@gmail.com>
2017-09-24 12:39:12 -0400

    moving folders

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  // in production, return prod set of keys
-  module.exports = require("./prod");
-} else {
-  // in dev - return dev keys
-  module.exports = require("./dev");
-}
--- config/prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
-  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN
-};

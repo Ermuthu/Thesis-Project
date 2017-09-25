9bff346749d8aed9a89502fd309dbe1f98c37592
Dan Martin <dmartss13@gmail.com>
2017-09-25 06:07:22 -0400

    initial states to use

---
+++ client/src/reducers/initialState.jsx
@@ -0,0 +1,53 @@
+export default {
+  currentSong: {
+    playUrl: "",
+    audio: null,
+    playing: false
+  },
+  youtube: {
+    videos: [],
+    term: ""
+  },
+  accessToken: null,
+  refreshToken: null,
+  user: {
+    loading: false,
+    display_name: null,
+    email: null,
+    external_urls: {},
+    followers: {},
+    href: null,
+    id: null,
+    images: [],
+    uri: null
+  },
+  artists: {
+    following: {
+      items: [],
+      isFetching: false,
+      error: false
+    }
+  },
+  songs: {
+    following: {
+      items: [],
+      isFetching: false,
+      error: false
+    }
+  },
+  genre: {
+    following: {
+      items: [],
+      isFetching: false,
+      error: false
+    }
+  },
+  playlists: {
+    following: {
+      items: [],
+      isFetching: false,
+      error: false,
+      errorMessage: null
+    }
+  }
+};

ef123327986fdb623637bfd6676b69a4bdb291be
Dan Martin <dmartss13@gmail.com>
2017-09-25 05:02:43 -0400

    youtube files

---
+++ client/src/components/youtube/Youtube.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+
+class Youtube extends Component {
+  render() {
+    return <SearchBar />;
+  }
+}
+
+export default Youtube;

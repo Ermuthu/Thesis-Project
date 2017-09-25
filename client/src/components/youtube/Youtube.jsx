5df2fa277d8046ef10682cfc26ebcd31ca6ae8b0
Dan Martin <dmartss13@gmail.com>
2017-09-25 06:09:02 -0400

    main youtube component with youtube state logging as props

--- client/src/components/youtube/Youtube.jsx
+++ client/src/components/youtube/Youtube.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 
 class Youtube extends Component {
   render() {
-    return <SearchBar />;
+    const { youtube } = this.props;
+    console.log(youtube);
+    return (
+      <div>
+        <SearchBar />
+        <VideoList />
+      </div>
+    );
   }
 }
 
-export default Youtube;
+function mapStateToProps({ youtube }) {
+  return { youtube };
+}
+
+export default connect(mapStateToProps)(Youtube);

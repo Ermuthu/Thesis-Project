1b49b0bfc546360666a5d6a7c3d635b6e3130e2a
Dan Martin <dmartss13@gmail.com>
2017-09-25 04:56:44 -0400

    with users info now in mongo db prepping to personalize page for user

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,29 +1,33 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Header from "../Header/Header";
 import { Nav, SpL, YtL, Img, SpI, YtI } from "./Home.Style";
 
 class Home extends Component {
-  renderHeader() {
-    switch (this.props.auth) {
-      case null:
-        return "loading...";
-      default:
-        return (
-          <div>
-            <Header />
-            <a href="/api/logout">
-              <i className="large material-icons right">exit_to_app</i>Logout
-            </a>
-          </div>
-        );
-    }
+  renderProfile() {
+    const { auth } = this.props;
+    // const profile = auth.playlists;
+    console.log(auth);
+    // conso;
+    //   {
+    //     auth.map((item, index) => {
+    //       return (
+    //         <div key={index}>
+    //           <div>
+    //             <div>{item.name}</div>
+    //             <img src={item.images[0].url} alt="" />
+    //           </div>
+    //         </div>
+    //       );
+    //     });
   }
+  // }
   render() {
+    const { auth } = this.props;
     return (
       <div>
-        {this.props.auth ? (
+        {auth ? (
           <Nav>
+            {this.renderProfile()}
             <div>
               <SpI>
                 <SpL to={"/spotify"}>Search Spotify</SpL>

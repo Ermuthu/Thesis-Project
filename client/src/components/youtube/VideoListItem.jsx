61a80eb1cb19d6176b090d9e447bf5faf081ba48
Dan Martin <dmartss13@gmail.com>
2017-09-25 09:49:19 -0400

    videos are playable

--- client/src/components/youtube/VideoListItem.jsx
+++ client/src/components/youtube/VideoListItem.jsx
@@ -1,14 +1,29 @@
 import React from "react";
-// import { connect } from "react-redux";
+import { connect } from "react-redux";
 
 const VideoListItem = ({ youtube, video }) => {
+  const videoId = video.id.videoId;
+  const url = `https:www.youtube.com/embed/${videoId}`;
+  const imageUrl = video.snippet.thumbnails.default.url;
   // console.log(youtube);
   // console.log(video);
-  return <li> </li>;
+  // console.log(url);
+  // console.log(videoId);
+  return (
+    <li className="container">
+      <iframe className="embed-responsive-item" src={url} />
+      <div>
+        <div className="media-heading">
+          {video.snippet.title}
+          {video.snippet.description}
+        </div>
+      </div>
+    </li>
+  );
 };
 
-// function mapStateToProps({ youtube }) {
-//   return { youtube };
-// }
+function mapStateToProps({ youtube }) {
+  return { youtube };
+}
 
-export default VideoListItem;
+export default connect(mapStateToProps)(VideoListItem);

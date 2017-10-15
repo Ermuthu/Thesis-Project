811191494aa9329382abf2f4d9a0a1dae26ac7f0
Dan Martin <dmartss13@gmail.com>
2017-10-15 14:01:21 -0400

    auth with new request and logout

--- client/src/actions/auth.jsx
+++ client/src/actions/auth.jsx
@@ -1,7 +1,13 @@
 import axios from "axios";
 import * as actions from "./constants";
 
+export const loginRequest = () => {
+  return { type: actions.LOGIN_REQUEST };
+};
+
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
   dispatch({ type: actions.FETCH_USER, payload: res.data });
 };
+
+export const logOut = () => ({ type: actions.LOGOUT });

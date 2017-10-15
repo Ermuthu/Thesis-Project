6eb4ea841e00a81e5d73c290bbaa342db10b058d
Dan Martin <dmartss13@gmail.com>
2017-10-15 11:59:41 -0400

    store and root divided

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,10 @@
 import "materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import "./App.css";
-import App from "./components/App";
-import { store, rootElement } from "./createStore";
+import configureStore from "./configureStore";
+import Root from "./Root";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
-);
+const store = configureStore();
+
+ReactDOM.render(<Root store={store} />, document.querySelector("#root"));

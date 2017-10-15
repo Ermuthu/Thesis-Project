8e325e5b82f8ed781c29fd43079426d4a2325494
Dan Martin <dmartss13@gmail.com>
2017-10-15 14:03:14 -0400

    artist reducer with new initial

--- client/src/reducers/reducer_artist.jsx
+++ client/src/reducers/reducer_artist.jsx
@@ -1,11 +1,24 @@
-import { FETCH_ARTIST } from "../actions/constants";
+import { FETCH_ARTIST, CLEAR_SEARCH } from "../actions/constants";
 // import initialState from "./InitialState";
 
-export default function(state = null, action) {
+const initialState = {
+  error: {},
+  loading: false,
+  items: [],
+  success: false
+};
+
+export default function(state = initialState, action) {
   // console.log("reducer fetch artist -> ", action.payload);
   switch (action.type) {
     case FETCH_ARTIST:
-      return action.payload;
+      return {
+        ...state,
+        items: action.payload.items,
+        success: true
+      };
+    case CLEAR_SEARCH:
+      return initialState;
     default:
       return state;
   }

8ee38783fd68a0d86e2c5a3d207d48ce47411b31
Dan Martin <dmartss13@gmail.com>
2017-10-15 14:03:55 -0400

    request and logout reducers added to auth

--- client/src/reducers/reducer_auth.jsx
+++ client/src/reducers/reducer_auth.jsx
@@ -1,17 +1,26 @@
 import * as actions from "../actions/constants";
 
 const initialState = {
-  isLoggedIn: false,
-  isFetching: false,
-  isAuthenticated: localStorage.getItem("access_token" ? true : false)
+  loggedIn: false,
+  error: {},
+  loading: false,
+  auth: false,
+  success: false
 };
 
 export default function(state = initialState, action) {
   // console.log(state);
   // console.log("auth reducer", action.payload);
   switch (action.type) {
+    case actions.LOGIN_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
     case actions.FETCH_USER:
       return action.payload || false;
+    case actions.LOGOUT:
+      return false;
     default:
       return state;
   }

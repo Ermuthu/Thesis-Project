c68bd5565338f9fbf20d42c98729b89947ccdf37
Dan Martin <dmartss13@gmail.com>
2017-10-15 14:01:52 -0400

    more actions

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -1,14 +1,20 @@
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+
+export const API = "API";
 export const FETCH_USER = "FETCH_USER";
+export const LOGOUT = "LOGOUT";
 export const SET_TOKEN = "SET_TOKEN";
 
 export const FETCH_ARTIST = "FETCH_ARTIST";
+export const FETCH_FAILED = "FETCH_FAILED";
 export const FETCH_SONG = "FETCH_SONG";
 export const FETCH_PLAYLIST = "FETCH_PLAYLIST";
 export const FETCH_GENRE = "FETCH_GENRE";
 export const SELECTED_PLAYLIST = "SELECTED_PLAYLIST";
 
-export const ADD_SEARCH = "ADD_SEARCH";
-
-export const ADD_LOG = "ADD_LOG";
 export const CLEAR_SEARCH = "CLEAR_SEARCH";
 export const FETCH_YOUTUBE = "FETCH_YOUTUBE";
+
+export const START_NETWORK = "START_NETWORK";
+export const END_NETWORK = "END_NETWORK";
+export const SWITCH_THEME = "SWITCH_THEME";

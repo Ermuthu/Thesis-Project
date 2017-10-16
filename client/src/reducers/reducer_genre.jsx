47bc3fe1a2f07aaa78296ad64a372b34d0923e08
Dan Martin <dmartss13@gmail.com>
2017-10-16 18:26:04 -0400

    genre mapping in reducer

--- client/src/reducers/reducer_genre.jsx
+++ client/src/reducers/reducer_genre.jsx
@@ -2,13 +2,22 @@ import * as actions from "../actions/constants";
 import initialState from "./state_initial";
 
 export default function(state = initialState.spotify, action) {
-  // console.log("reducer fetch genre -> ", action.payload);
   switch (action.type) {
     case actions.FETCH_GENRE:
       return {
         ...state,
-        items: action.payload.items,
+        items: action.payload.items.map(item => {
+          return {
+            trackImg: item.images[0].url,
+            name: item.name,
+            href: item.tracks.href,
+            id: item.id
+          };
+        }),
         success: true,
+        selectedGenre: [],
+        selectedArtist: [],
+        selectedPlaylist: [],
         error: null,
         isLoading: false
       };
@@ -16,18 +25,23 @@ export default function(state = initialState.spotify, action) {
       return {
         ...state,
         items: [],
-        selectedGenre: action.payload.items,
+        selectedGenre: action.payload.items.map(item => {
+          return {
+            track: item.track,
+            album: item.track.album,
+            trackImg: item.track.album.images[0].url,
+            id: item.track.id,
+            preview_url: item.track.preview_url
+          };
+        }),
+        selectedArtist: [],
+        selectedPlaylist: [],
         success: true,
         error: null,
         isLoading: false
       };
     case actions.CLEAR_SEARCH:
-      return {
-        ...state,
-        success: false,
-        items: [],
-        selectedGenre: []
-      };
+      return initialState;
     default:
       return state;
   }

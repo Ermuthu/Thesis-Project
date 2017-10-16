0c75daf2e3bdb33dfa380568254a6c2df8eebc8a
Dan Martin <dmartss13@gmail.com>
2017-10-16 15:38:27 -0400

    cleaned up spotify playlist a lot

--- client/src/components/spotify/Playlist.jsx
+++ client/src/components/spotify/Playlist.jsx
@@ -1,48 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { selectedPlaylist } from "../../actions/spotify";
 import { connect } from "react-redux";
-import { PlaylistContainer } from "./Spotify.Style";
+import { PlaylistContainer, Image } from "./Spotify.Style";
 
-class SpotifyPlaylist extends Component {
-  renderPlaylist() {
-    const { playlist } = this.props;
-    const list = playlist.items;
-    // console.log("playlist at render", list);
-    return (
-      <div>
-        {list.map((item, index) => {
-          const trackImg = item.images[0].url;
-          const url = item.tracks.href;
-          return (
-            <PlaylistContainer
-              key={item.id}
-              onClick={() => this.props.selectedPlaylist(url)}
-            >
-              <Link to={`/spotify/playlist/${item.id}`}>
-                <div>{item.name}</div>
-                <img
-                  src={trackImg}
-                  alt="track"
-                  style={{ width: "200px", height: "200px" }}
-                />
-              </Link>
-            </PlaylistContainer>
-          );
-        })}
-      </div>
-    );
-  }
-
-  render() {
-    const { playlist } = this.props;
-    if (!playlist) {
-      return <div> Loading ... </div>;
-    }
-    return <div>{this.renderPlaylist()}</div>;
-  }
-}
-
+const SpotifyPlaylist = ({ playlist, selectedPlaylist }) => (
+  <div>
+    {playlist.items.map(item => (
+      <PlaylistContainer
+        key={item.id}
+        onClick={() => selectedPlaylist(item.url)}
+      >
+        <Link to={`/spotify/playlist/${item.name}`}>
+          <div>{item.name}</div>
+          <Image src={item.trackImg} alt="track" />
+        </Link>
+      </PlaylistContainer>
+    ))};
+  </div>
+);
 const mapStateToProps = ({ playlist }) => ({ playlist });
 
 export default connect(mapStateToProps, { selectedPlaylist })(SpotifyPlaylist);

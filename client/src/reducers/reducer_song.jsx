254f2645ebd32fe9139c8f3f4f478c6337860b33
Dan Martin <dmartss13@gmail.com>
2017-10-16 18:26:11 -0400

    mapping in song reducer

--- client/src/reducers/reducer_song.jsx
+++ client/src/reducers/reducer_song.jsx
@@ -7,17 +7,22 @@ export default function(state = initialState.spotify, action) {
     case FETCH_SONG:
       return {
         ...state,
-        items: action.payload.items,
+        items: action.payload.items.map(item => {
+          return {
+            name: item.name,
+            id: item.id,
+            preview_url: item.preview_url,
+            uri: item.uri,
+            album: item.album,
+            trackImg: item.album.images[0].url
+          };
+        }),
         success: true,
         error: null,
         isLoading: false
       };
     case CLEAR_SEARCH:
-      return {
-        ...state,
-        success: false,
-        items: []
-      };
+      return initialState;
     default:
       return state;
   }

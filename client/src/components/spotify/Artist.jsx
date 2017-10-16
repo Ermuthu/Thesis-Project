e58462d8a0f2afecb4521ec07224f826b46e3e1b
Dan Martin <dmartss13@gmail.com>
2017-10-16 18:21:47 -0400

    refactor artist to stateless

--- client/src/components/spotify/Artist.jsx
+++ client/src/components/spotify/Artist.jsx
@@ -1,44 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { ArtistContainer } from "./Spotify.Style";
-import { fetchArtist, clearSearch } from "../../actions/spotify";
+import { Link } from "react-router-dom";
+import { ArtistContainer, Image } from "./Spotify.Style";
+import { selectedArtist } from "../../actions/spotify";
 
-class SpotifyArtist extends Component {
-  componentWillUnmount = () => this.props.clearSearch();
-
-  render() {
-    const { artist, clearSearch } = this.props;
-    if (!artist) {
-      return <div> Loading ... </div>;
-    }
-    const list = artist.items ? artist.items : [];
-    // console.log(list);
-    // console.log(artist);
-    return (
-      <ArtistContainer>
-        {list.map((item, index) => {
-          const name = item.name;
-          const artistImg = item.images[0] ? item.images[0].url : "";
-          return (
-            <div key={index}>
-              <div>
-                <div>{name}</div>
-                <img
-                  src={artistImg}
-                  alt="artist"
-                  style={{ width: "200px", height: "200px" }}
-                />
-              </div>
-            </div>
-          );
-        })}
+const SpotifyArtist = ({ artist, selectedArtist }) => (
+  <div>
+    {artist.items.map(artist => (
+      <ArtistContainer
+        key={artist.id}
+        onClick={() => selectedArtist(artist.href)}
+      >
+        <Link to={`/spotify/artist/${artist.name}`}>
+          <div>{artist.name}</div>
+          <Image src={artist.trackImg} alt="artist" />
+        </Link>
       </ArtistContainer>
-    );
-  }
-}
+    ))}
+  </div>
+);
 
 const mapStateToProps = ({ artist }) => ({ artist });
 
-export default connect(mapStateToProps, { fetchArtist, clearSearch })(
-  SpotifyArtist
-);
+export default connect(mapStateToProps, { selectedArtist })(SpotifyArtist);

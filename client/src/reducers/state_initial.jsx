5576275ad7bfb63d96cb4f985b7b33f080940f0f
Dan Martin <dmartss13@gmail.com>
2017-10-16 15:40:56 -0400

    initial state improvements

--- client/src/reducers/state_initial.jsx
+++ client/src/reducers/state_initial.jsx
@@ -11,42 +11,22 @@ export default {
   refreshToken: null,
   user: {
     loading: false,
-    display_name: null,
-    email: null,
-    external_urls: {},
-    followers: {},
-    href: null,
-    id: null,
-    images: [],
-    uri: null
+    isLoggedIn: false,
+    isFetching: false,
+    isAuthenticated: sessionStorage.getItem("accessToken" ? true : false),
+    spotifyId: null,
+    acessToken: null,
+    refreshToken: null,
+    profile: {},
+    _id: null
   },
-  artists: {
-    following: {
-      items: [],
-      isFetching: false,
-      error: false
-    }
-  },
-  songs: {
-    following: {
-      items: [],
-      isFetching: false,
-      error: false
-    }
-  },
-  genre: {
-    following: {
-      items: [],
-      isFetching: false,
-      error: false
-    }
-  },
-  playlists: {
-    following: {
-      items: [],
-      isFetching: false,
-      error: false,
-      errorMessage: null
-    }
+  spotify: {
+    error: {},
+    isLoading: false,
+    items: [],
+    success: false,
+    input: "",
+    selectedPlaylist: [],
+    selectedGenre: []
   }
 };

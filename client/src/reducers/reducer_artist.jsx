8a8c46fd974cb5ac1ee0ce1e26013462cbd8e504
Dan Martin <dmartss13@gmail.com>
2017-10-16 18:23:32 -0400

    mapping in artist reducer

--- client/src/reducers/reducer_artist.jsx
+++ client/src/reducers/reducer_artist.jsx
@@ -1,24 +1,48 @@
-import { FETCH_ARTIST, CLEAR_SEARCH } from "../actions/constants";
+import * as actions from "../actions/constants";
 import initialState from "./state_initial";
 
 export default function(state = initialState.spotify, action) {
   // console.log("reducer fetch artist -> ", action.payload);
   switch (action.type) {
-    case FETCH_ARTIST:
+    case actions.FETCH_ARTIST:
       return {
         ...state,
-        items: action.payload.items,
+        items: action.payload.items.map(item => {
+          return {
+            name: item.name,
+            trackImg: item.images[0] ? item.images[0].url : item.name,
+            uri: item.uri,
+            href: item.href,
+            id: item.id
+          };
+        }),
+        selectedGenre: [],
+        selectedArtist: [],
+        selectedPlaylist: [],
         success: true,
-        input: action.payload.input,
         error: null,
         isLoading: false
       };
-    case CLEAR_SEARCH:
+    case actions.SELECTED_ARTIST:
       return {
         ...state,
-        success: false,
-        items: []
+        items: [],
+        selectedGenre: [],
+        selectedPlaylist: [],
+        selectedArtist: {
+          external_urls: action.payload.external_urls.spotify,
+          name: action.payload.name,
+          img: action.payload.images[0].url,
+          uri: action.payload.uri,
+          id: action.payload.id,
+          message: ", Click for artists Spotify profile"
+        },
+        success: true,
+        error: null,
+        isLoading: false
       };
+    case actions.CLEAR_SEARCH:
+      return initialState;
     default:
       return state;
   }

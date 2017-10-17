49cf0f6dc49905c9c96af018bf0fd11982e1a814
Dan Martin <dmartss13@gmail.com>
2017-10-17 16:38:58 -0400

    re did spotify main to stateless, still need to split it up though, but added redux form

--- client/src/components/spotify/Spotify.jsx
+++ client/src/components/spotify/Spotify.jsx
@@ -1,77 +1,66 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+import { withRouter } from "react-router-dom";
 import * as actions from "../../actions/spotify";
-import { Link } from "react-router-dom";
 
-class Spotify extends Component {
-  state = { input: "" };
+const Spotify = ({
+  requestArtist,
+  requestSong,
+  requestGenre,
+  requestPlaylist,
+  handleSubmit,
+  clearSearch,
+  history
+}) => {
+  return (
+    <div>
+      <form onSubmit={handleSubmit(requestArtist)}>
+        <Field
+          name="artist"
+          type="input"
+          component="input"
+          label="search by artist"
+        />
+        <button onClick={() => history.push("/spotify/artist")} />
+      </form>
+      <form onSubmit={handleSubmit(requestSong)}>
+        <Field
+          name="song"
+          type="input"
+          component="input"
+          label="search by song"
+        />
+        <button onClick={() => history.push("/spotify/song")} />
+      </form>
+      <form onSubmit={handleSubmit(requestGenre)}>
+        <Field
+          name="genre"
+          type="input"
+          component="input"
+          label="search by genre"
+        />
+        <button onClick={() => history.push("/spotify/genre")} />
+      </form>
+      <form onSubmit={handleSubmit(requestPlaylist)}>
+        <Field
+          name="playlist"
+          type="input"
+          component="input"
+          label="search by playlist"
+        />
+        <button onClick={() => history.push("/spotify/playlist")} />
+      </form>
+      <button
+        onSubmit={handleSubmit(clearSearch)}
+        onClick={() => history.push("/spotify")}
+      >
+        Clear Your Search
+      </button>
+    </div>
+  );
+};
 
-  render() {
-    const {
-      auth,
-      fetchArtist,
-      fetchSong,
-      fetchGenre,
-      fetchPlaylist
-    } = this.props;
-    let input = this.state.input;
-    return (
-      <div>
-        {auth ? (
-          <div>
-            <input
-              style={{
-                width: "300px",
-                textAlign: "center"
-              }}
-              className="form-control"
-              placeholder="search by artist or song"
-              onChange={event =>
-                this.setState({
-                  input: event.target.value
-                })}
-            />
-            <Link
-              to={"/spotify/artist"}
-              className="btn btn-success"
-              disabled={!this.state.input}
-              onClick={() => fetchArtist(input)}
-            >
-              Search for an Artist
-            </Link>
-            <Link
-              to={"/spotify/song"}
-              disabled={!this.state.input}
-              className="btn btn-success"
-              onClick={() => fetchSong(input)}
-            >
-              Search for a Song!
-            </Link>
-            <Link
-              to="/spotify/playlist"
-              disabled={!this.state.input}
-              className="btn btn-success"
-              onClick={() => fetchPlaylist(input)}
-            >
-              Search for a Playlist
-            </Link>
-            <Link
-              to="/spotify/genre"
-              disabled={!this.state.input}
-              className="btn btn-success"
-              onClick={() => fetchGenre(input)}
-            >
-              Search for a Genre
-            </Link>
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = ({ auth }) => ({ auth });
-
-export default connect(mapStateToProps, actions)(Spotify);
+export default reduxForm({
+  form: "SpotifyForm"
+})(connect(null, actions)(withRouter(Spotify)));

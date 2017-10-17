100898221e714488426e9c8228be20b8349f8a34
Dan Martin <dmartss13@gmail.com>
2017-10-17 16:38:40 -0400

    better action creators

--- client/src/actions/spotify.jsx
+++ client/src/actions/spotify.jsx
@@ -7,15 +7,16 @@ const headers = {
 };
 const fetch = { method: "get", headers: headers };
 
-export const spotifyRequest = () => ({
-  type: actions.SPOTIFY_REQUEST,
-  isLoading: true,
-  error: null
+export const requestArtist = artist => dispatch => ({
+  type: actions.REQUEST_ARTIST,
+  meta: {
+    throttle: 5000
+  },
+  onSuccess: {
+    type: actions.ARTIST_SUCCESS,
+    dispatch: dispatch(fetchArtist(artist.artist))
+  }
 });
-
-export const requestArtist = artist => dispatch => {
-  dispatch(fetchArtist(artist.artist));
-};
 
 export const fetchArtist = artist => async dispatch => {
   const FETCH_URL = `${BASE_URL}q=${artist}&type=artist`;
@@ -28,15 +29,31 @@ export const selectedArtist = href => async dispatch => {
   dispatch({ type: actions.SELECTED_ARTIST, payload: res.data });
 };
 
-export const fetchSong = input => async dispatch => {
-  const FETCH_URL = `${BASE_URL}q=${input}&type=track`;
+export const requestSong = song => dispatch => ({
+  type: actions.REQUEST_SONG,
+  onSuccess: {
+    type: actions.SONG_SUCCESS,
+    dispatch: dispatch(fetchSong(song.song))
+  }
+});
+
+export const fetchSong = song => async dispatch => {
+  const FETCH_URL = `${BASE_URL}q=${song}&type=track`;
   const res = await axios.get(FETCH_URL, fetch);
   dispatch({ type: actions.FETCH_SONG, payload: res.data.tracks });
   // console.log(res);
 };
 
-export const fetchPlaylist = input => async dispatch => {
-  const FETCH_URL = `${BASE_URL}q=${input}&type=playlist`;
+export const requestPlaylist = playlist => dispatch => ({
+  type: actions.REQUEST_PLAYLIST,
+  onSuccess: {
+    type: actions.PLAYLIST_SUCCESS,
+    dispatch: dispatch(fetchPlaylist(playlist.playlist))
+  }
+});
+
+export const fetchPlaylist = playlist => async dispatch => {
+  const FETCH_URL = `${BASE_URL}q=${playlist}&type=playlist`;
   const res = await axios.get(FETCH_URL, fetch);
   dispatch({ type: actions.FETCH_PLAYLIST, payload: res.data.playlists });
   // console.log("playlist data", res.data.playlists.items);
@@ -48,8 +65,16 @@ export const selectedPlaylist = url => async dispatch => {
   dispatch({ type: actions.SELECTED_PLAYLIST, payload: res.data });
 };
 
-export const fetchGenre = input => async dispatch => {
-  const FETCH_URL = `${CATEGORY_URL}${input}/playlists`;
+export const requestGenre = genre => dispatch => ({
+  type: actions.REQUEST_GENRE,
+  onSuccess: {
+    type: actions.GENRE_SUCCESS,
+    dispatch: dispatch(fetchGenre(genre.genre))
+  }
+});
+
+export const fetchGenre = genre => async dispatch => {
+  const FETCH_URL = `${CATEGORY_URL}${genre}/playlists`;
   const res = await axios.get(FETCH_URL, fetch);
   dispatch({ type: actions.FETCH_GENRE, payload: res.data.playlists });
   // console.log(res.data.playlists);
@@ -61,3 +86,11 @@ export const selectedGenre = href => async dispatch => {
 };
 
 export const clearSearch = () => ({ type: actions.CLEAR_SEARCH });
+
+// export const spotifyRequest = () => ({
+//   type: actions.REQUEST_ARTIST,
+//   artist,
+//   history,
+//   url: `${BASE_URL}q=${artist.artist}&type=artist`,
+//   onSuccess: (res, dispatch) => dispatch(fetchArtist(artist, res.artist.items))
+// });

3284b5a49d936f8473ab1f0a458f22c53be9cbb0
Dan Martin <dmartss13@gmail.com>
2017-10-18 15:05:50 -0400

    cleaner layout for spotify

--- client/src/components/spotify/Spotify.jsx
+++ client/src/components/spotify/Spotify.jsx
@@ -1,56 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Field, reduxForm } from "redux-form";
+import { reduxForm } from "redux-form";
 import { withRouter } from "react-router-dom";
 import * as actions from "../../actions/spotify";
+import SpotifyField from "./FieldExport";
 
-const Spotify = ({
-  requestArtist,
-  requestSong,
-  requestGenre,
-  requestPlaylist,
-  handleSubmit,
-  clearSearch,
-  history
-}) => {
+const Spotify = ({ handleSubmit, clearSearch, history }) => {
   return (
     <div>
-      <form onSubmit={handleSubmit(requestArtist)}>
-        <Field
-          name="artist"
-          type="input"
-          component="input"
-          label="search by artist"
-        />
-        <button onClick={() => history.push("/spotify/artist")} />
-      </form>
-      <form onSubmit={handleSubmit(requestSong)}>
-        <Field
-          name="song"
-          type="input"
-          component="input"
-          label="search by song"
-        />
-        <button onClick={() => history.push("/spotify/song")} />
-      </form>
-      <form onSubmit={handleSubmit(requestGenre)}>
-        <Field
-          name="genre"
-          type="input"
-          component="input"
-          label="search by genre"
-        />
-        <button onClick={() => history.push("/spotify/genre")} />
-      </form>
-      <form onSubmit={handleSubmit(requestPlaylist)}>
-        <Field
-          name="playlist"
-          type="input"
-          component="input"
-          label="search by playlist"
-        />
-        <button onClick={() => history.push("/spotify/playlist")} />
-      </form>
+      <SpotifyField />
       <button
         onSubmit={handleSubmit(clearSearch)}
         onClick={() => history.push("/spotify")}

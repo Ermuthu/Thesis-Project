d41e6abcd50b4ee5e8d0ce5758c9c494b3b99480
Dan Martin <dmartss13@gmail.com>
2017-10-21 05:09:47 -0400

    refactor selected playlist to stateless

--- client/src/components/spotify/results/selected/SelectedPlaylist.jsx
+++ client/src/components/spotify/results/selected/SelectedPlaylist.jsx
@@ -1,68 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { clearSearch } from "../../../../actions/spotify";
-import { SongContainer, Inner, Image } from "../../Spotify.Style";
+import { SongsDisplay } from "../../containers/Headers";
 
-class SelectedPlaylist extends Component {
-  state = { playUrl: "", audio: null, playing: false };
-  componentWillUnmount = () => this.props.clearSearch();
+const SelectedPlaylist = ({ playlist }) => {
+  return (
+    <div>
+      {playlist.selectedPlaylist.map(item => (
+        <div key={item.uri}>
+          <SongsDisplay
+            trackImg={item.trackImg}
+            name={item.name}
+            uri={item.uri}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-  playSong(url) {
-    let audio = new Audio(url);
-    if (!this.state.playing) {
-      audio.play();
-      this.setState({
-        playing: true,
-        playUrl: url,
-        audio
-      });
-    } else {
-      if (this.state.playUrl === url) {
-        this.state.audio.pause();
-        this.setState({
-          playing: false
-        });
-      } else {
-        this.state.audio.pause();
-        audio.play();
-        this.setState({
-          playing: true,
-          playUrl: url,
-          audio
-        });
-      }
-    }
-  }
+const mapState = ({ playlist }) => ({ playlist });
 
-  render() {
-    const { playlist } = this.props;
-    if (!playlist) {
-      return <div> Loading ... </div>;
-    }
-    return (
-      <SongContainer>
-        {playlist.selectedPlaylist.map(item => {
-          return (
-            <div key={item.uri} onClick={() => this.playSong(item.preview_url)}>
-              {item.name}
-              <Inner>
-                {this.state.playing ? (
-                  <span>
-                    <i className="material-icons">pause</i>
-                  </span>
-                ) : (
-                  <span>&#9654;</span>
-                )}
-              </Inner>
-              <Image src={item.trackImg} alt="song" />
-            </div>
-          );
-        })}
-      </SongContainer>
-    );
-  }
-}
-
-const mapStateToProps = ({ playlist }) => ({ playlist });
-
-export default connect(mapStateToProps, { clearSearch })(SelectedPlaylist);
+export default connect(mapState)(SelectedPlaylist);
--- client/src/middleware/spotify.jsx
+++ client/src/middleware/spotify.jsx
@@ -1,21 +1,20 @@
 import * as actions from "../actions/constants";
-import { playSpotify, pauseSpotify } from "../actions/spotify";
+import { playSong, pauseSong } from "../actions/spotify";
 
 const spotifyMiddleware = ({ dispatch }) => next => action => {
   if (action.type !== actions.PLAY_SPOTIFY && actions.PAUSE_SPOTIFY) {
     return next(action);
   }
-
-  const { url, preview_url } = action.payload;
+  const { preview_url } = action.payload;
   console.log(preview_url);
   const audio = new Audio(preview_url);
   const time = action.meta.active;
   if (time) {
     audio.play();
   }
-  if (!time) {
-    audio.currentTime = 0;
-    dispatch(pauseSpotify(audio.pause()));
+  const noTime = action.meta.notActive;
+  if (noTime) {
+    audio.pause();
   }
   return next(action);
 };

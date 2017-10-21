1bc3fb3229dee91de84dcbb4654bdf3e982ca1e3
Dan Martin <dmartss13@gmail.com>
2017-10-21 05:08:52 -0400

    selected genre factored to stateless

--- client/src/components/spotify/results/selected/SelectedGenre.jsx
+++ client/src/components/spotify/results/selected/SelectedGenre.jsx
@@ -1,63 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { SongContainer, Inner, Image } from "../../Spotify.Style";
+import { SongsDisplay } from "../../containers/Headers";
 
-class SelectedGenre extends Component {
-  state = { playUrl: "", audio: null, playing: false };
+const SelectedGenre = ({ genre }) => {
+  return (
+    <div>
+      {genre.selectedGenre.map(item => (
+        <div key={item.id}>
+          <SongsDisplay
+            name={item.name}
+            trackImg={item.trackImg}
+            uri={item.uri}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-  playSong(url) {
-    let audio = new Audio(url);
-    if (!this.state.playing) {
-      audio.play();
-      this.setState({
-        playing: true,
-        playUrl: url,
-        audio
-      });
-    } else {
-      if (this.state.playUrl === url) {
-        this.state.audio.pause();
-        this.setState({
-          playing: false
-        });
-      } else {
-        this.state.audio.pause();
-        audio.play();
-        this.setState({
-          playing: true,
-          playUrl: url,
-          audio
-        });
-      }
-    }
-  }
+const mapState = ({ genre }) => ({ genre });
 
-  render() {
-    const { genre } = this.props;
-    return (
-      <SongContainer>
-        {genre.selectedGenre.map(item => {
-          return (
-            <div key={item.id} onClick={() => this.playSong(item.preview_url)}>
-              <div>{item.name}</div>
-              <Inner>
-                {this.state.playing ? (
-                  <span>
-                    <i className="material-icons">pause</i>
-                  </span>
-                ) : (
-                  <span>&#9654;</span>
-                )}
-              </Inner>
-              <Image src={item.trackImg} alt="song" />
-            </div>
-          );
-        })}
-      </SongContainer>
-    );
-  }
-}
-
-const mapStateToProps = ({ genre }) => ({ genre });
-
-export default connect(mapStateToProps)(SelectedGenre);
+export default connect(mapState)(SelectedGenre);

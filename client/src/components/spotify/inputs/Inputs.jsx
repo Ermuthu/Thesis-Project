44909863476ffe03f455be9c1de4d6ef9b1ab1d6
Dan Martin <dmartss13@gmail.com>
2017-10-22 14:17:05 -0400

    move inputs

---
+++ client/src/components/spotify/inputs/Inputs.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Field } from "redux-form";
+import renderInput from "./renderInput";
+
+export const Artist = () => (
+  <Field
+    component={renderInput}
+    type="text"
+    name="artist"
+    placeholder="Search By Artist"
+  />
+);
+
+export const Song = () => (
+  <Field
+    component={renderInput}
+    type="text"
+    name="song"
+    placeholder="search by Song"
+  />
+);
+
+export const Genre = () => (
+  <Field
+    component={renderInput}
+    type="text"
+    name="genre"
+    placeholder="Search by Genre"
+  />
+);
+
+export const Playlist = () => (
+  <Field
+    component={renderInput}
+    type="text"
+    name="playlist"
+    placeholder="Search by Playlist"
+  />
+);

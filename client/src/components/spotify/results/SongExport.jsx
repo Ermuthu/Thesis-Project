5b8109a13788fbbab4deaa245e23c9fb90f4acf5
Dan Martin <dmartss13@gmail.com>
2017-10-21 22:23:25 -0400

    song export proptopes

--- client/src/components/spotify/results/SongExport.jsx
+++ client/src/components/spotify/results/SongExport.jsx
@@ -1,8 +1,26 @@
 import { connect } from "react-redux";
-import { playSong, pauseSong } from "../../../actions/spotify";
+import { playSong, pauseSong, nextTwenty } from "../../../actions/spotify";
 import { bindActionCreators } from "redux";
 import SpotifySong from "./Song";
+import PropTypes from "prop-types";
 
-const mapState = ({ song }) => ({ song });
+const mapState = ({ song }) => ({ song, next: song.results.next });
 
-export default connect(mapState, { playSong, pauseSong })(SpotifySong);
+SpotifySong.PropTypes = {
+  song: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      trackImg: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      uri: PropTypes.string.isRequired,
+      preview_url: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired,
+  playSong: PropTypes.func,
+  pauseSong: PropTypes.func,
+  nextTwenty: PropTypes.func
+};
+
+export default connect(mapState, { playSong, pauseSong, nextTwenty })(
+  SpotifySong
+);

781ef4c6a93fb8c9c92911f943095ec8a01983a9
Dan Martin <dmartss13@gmail.com>
2017-10-27 11:47:05 -0400

    changed flow so change constants

--- client/src/actions/constants.js
+++ client/src/actions/constants.js
@@ -1,44 +1,46 @@
+// auth
 export const LOGIN_REQUEST = "LOGIN_REQUEST";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 export const REFRESH_TOKEN = "REFRESH_TOKEN";
 
-export const API = "API";
+// auth is logged in
 export const FETCH_USER = "FETCH_USER";
 export const LOGOUT = "LOGOUT";
 
-export const SPOTIFY_REQUEST = "SPOTIFY_REQUEST";
-
+//artist
 export const REQUEST_ARTIST = "REQUEST_ARTIST";
-export const FETCH_ARTIST = "FETCH_ARTIST";
-export const SELECTED_ARTIST = "SELECTED_ARTIST";
 export const ARTIST_SUCCESS = "ARTIST_SUCCESS";
+export const ARTIST_FAILED = "ARTIST_FAILED";
+export const SELECTED_ARTIST = "SELECTED_ARTIST";
 
-export const SONG_FETCH_FAILED = "SONG_FETCH_FAILED";
-export const ARTIST_FETCH_FAILED = "ARTIST_FETCH_FAILED";
-export const GENRE_FETCH_FAILED = "FETCH_FAILED";
-export const PLAYLIST_FETCH_FAILED = "PLAYLIST_FETCH_FAILED";
-
-export const FETCH_SONG = "FETCH_SONG";
+//song
 export const REQUEST_SONG = "REQUEST_SONG";
 export const SONG_SUCCESS = "SONG_SUCCESS";
+export const SONG_FAILED = "SONG_FAILED";
 
+//playlist
 export const REQUEST_PLAYLIST = "REQUEST_PLAYLIST";
-export const FETCH_PLAYLIST = "FETCH_PLAYLIST";
-export const SELECTED_PLAYLIST = "SELECTED_PLAYLIST";
 export const PLAYLIST_SUCCESS = "PLAYLIST_SUCCESS";
+export const PLAYLIST_FAILED = "PLAYLIST_FAILED";
+export const SELECTED_PLAYLIST = "SELECTED_PLAYLIST";
 
+//genre
 export const REQUEST_GENRE = "REQUEST_GENRE";
-export const FETCH_GENRE = "FETCH_GENRE";
-export const SELECTED_GENRE = "SELECTED_GENRE";
 export const GENRE_SUCCESS = "GENRE_SUCCESS";
+export const GENRE_FAILED = "GENRE_FAILED";
+export const SELECTED_GENRE = "SELECTED_GENRE";
 
 export const CLEAR_SEARCH = "CLEAR_SEARCH";
 
+// youtube searching
 export const FETCH_YOUTUBE = "FETCH_YOUTUBE";
 export const YOUTUBE_SUCCESS = "YOUTUBE_SUCCESS";
 export const REQUEST_YOUTUBE = "REQUEST_YOUTUBE";
 
+// song player
 export const PLAY_SONG = "PLAY_SONG";
 export const PLAY_SPOTIFY = "PLAY_SPOTIFY";
 export const PAUSE_SPOTIFY = "PAUSE_SPOTIFY";
+
+export const ERROR = "ERROR";
